813faeddb94a5a41609bde4f7a74473354c1d4d6
Lucca Beurmann <luccab.souza@gmail.com>
2022-02-19 17:02:03 -0300

    refact: adjust white color

--- core-ui/src/main/java/br/com/alaksion/core_ui/theme/Theme.kt
+++ core-ui/src/main/java/br/com/alaksion/core_ui/theme/Theme.kt
@@ -3,16 +3,16 @@ package br.com.alaksion.core_ui.theme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 
 private val LightColorPalette = lightColors(
     primary = Orange,
     primaryVariant = LightOrange,
-    onPrimary = AppWhite,
+    onPrimary = Color.White,
     secondary = Blue,
     secondaryVariant = LightBlue,
-    onSecondary = AppWhite,
-    background = AppWhite,
+    onSecondary = Color.White,
     onBackground = AppBlack,
 )
 
